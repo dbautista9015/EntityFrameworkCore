a38f3ea3ec1a4d9b8b7fc902a92c281894971c25
Daniel Bautista <dbautista9015@gmail.com>
2023-10-17 17:54:42 -0700

    Add reference to Data and install EF Core

---
+++ EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -0,0 +1,10 @@
+using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Data;
+
+public class FootballLeagueDbContext : DbContext
+{
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<Coach> Coaches { get; set; }
+}
