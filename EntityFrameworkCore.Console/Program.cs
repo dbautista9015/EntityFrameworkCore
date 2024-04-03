fba24c1d910a3dd9a94c5ac9c530486ccf055368
Daniel Bautista <dbautista9015@gmail.com>
2024-04-03 16:54:15 -0700

    Fix database location, and use GetAllTeams

--- EntityFrameworkCore.Console/Program.cs
+++ EntityFrameworkCore.Console/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using EntityFrameworkCore.Data;
 
-Console.WriteLine("Hello, World!");
+// First we need an instance of the context
+using var context = new FootballLeagueDbContext();
+
+GetAllTeams();
+
+void GetAllTeams()
+{
+    // SELECT * FROM teams
+    var teams = context.Teams.ToList();
+
+    foreach (var team in teams)
+    {
+        Console.WriteLine(team.Name);
+    }
+}
--- EntityFrameworkCore.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -1,17 +1,32 @@
 using EntityFrameworkCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EntityFrameworkCore.Data;
 
 public class FootballLeagueDbContext : DbContext
 {
+    public FootballLeagueDbContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Combine(path, "FootballLeague_EfCore.db");
+    }
+
+
     public DbSet<Team> Teams { get; set; }
     public DbSet<Coach> Coaches { get; set; }
+    public string DbPath { get; private set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
-        optionsBuilder.UseSqlite($"Data Source=FootballLeague_EfCore.db");
+        // Do not use EnableSensitiveData and Detailed for production
+        // Only for educational purposes
+        optionsBuilder.UseSqlite($"Data Source={DbPath}")
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
